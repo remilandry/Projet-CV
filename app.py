37acd546682efdac007baa87e8d1c559bf51070b
Remi Landry <remi.landry@skema.edu>
2025-10-01 00:51:06 +0200

    Ajout code initial Streamlit

---
+++ app.py
@@ -0,0 +1,5 @@
+# app.py
+import streamlit as st  # # On importe Streamlit
+
+st.title("Mon Projet Final")  # # Affiche un titre
+st.write("Bienvenue sur mon application Streamlit !")  # # Affiche un texte
